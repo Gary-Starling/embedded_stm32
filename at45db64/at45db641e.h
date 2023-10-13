d233c8aef8c0ad4ab8d6581054361888cfb5fd94
Игорь <skvorec92@mail.ru>
2023-10-13 10:46:10 +0300

    Update at45db641e.h

--- at45db64/at45db641e.h
+++ at45db64/at45db641e.h
@@ -1,33 +1,35 @@
 #ifndef _AT45DB641E_H_
 #define _AT45DB641E_H_
 
+/* Memory chip module AT45DB641E (64-Mbit/8Mbyte)*/
 
-/* Модуль работы с микросхемой памяти AT45DB641E (64-Mbit/8Mbyte)*/
-
-/* Весь функционал сделан для 256 bytes страниц.
-Пожалуйста, перед использованием воспользуйтесь
+/*
+All functionality is made for 256 bytes pages.
+Please use
 -> at45_ps_read()  и  at45_set_ps(code);
+before start
 */
 
 
 #include <inttypes.h>
+#include <stddef.h>
 
 /*!!!using the binary page size (256 bytes)!!!*/
+//#define DEBUG_AT45
 
-
-//закоментировать чтобы убрать отладочный вывод
-//#define DEBUG_AT45        
-
-//Если включен данный макрос, то используется osDelay
-//Если нет, то HAL_Delay
+//If this macro is enabled, then osDelay is used; if not, then delay_ms()
 #define RTOS
 
-/* Установить используемые входы и выходы */
-/*  SPI1  STM32F405RG   
+/*  SPI1  STM32F405RG
 PC3     ------> CS
 PA5     ------> SPI1_SCK
 PA6     ------> SPI1_MISO
 PA7     ------> SPI1_MOSI   */
+
+#define GPIO_PIN_3     ((uint16_t)0x0008)
+#define GPIO_PIN_5     ((uint16_t)0x0020)
+#define GPIO_PIN_6     ((uint16_t)0x0040)
+#define GPIO_PIN_7     ((uint16_t)0x0080)
 
 #define CS_PIN          GPIO_PIN_3
 #define CS_PORT         GPIOC
@@ -37,15 +39,14 @@ PA7     ------> SPI1_MOSI   */
 #define SPI1_MOSI       GPIO_PIN_7
 #define SPI1_PORT       GPIOA
 
-#define CS_SET          HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_SET);
-#define CS_RESET        HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_RESET);
+#define CS_SET          (CS_PORT->BSRR = GPIO_PIN_3)
+#define CS_RESET        (CS_PORT->BSRR = (uint32_t)GPIO_PIN_3 << 16U)
 
-//Тайминги
+//timing
 #define TIME_OUT_SPI    (60U)
 #define MAX_PAGE_TIME   (5U)    //doc 3ms
-#define MAX_BYTE_TIME   (1U)    //doc 1us ! HAL == 1msec
+#define MAX_BYTE_TIME   (100U)    //doc 1us ! tim1 == 1msec
 #define MAX_PAGE_ER_T   (40U)   //doc 35ms
-
 
 //Commands(opcods)
 #define READ_ID         ((uint8_t)(0x9F))       //read identification information
@@ -53,7 +54,7 @@ PA7     ------> SPI1_MOSI   */
 #define CAR_HFM         ((uint8_t)(0x0B))       //Continuous Array Read (High Frequency Mode: 0Bh Opcode)
 
 #define SIZE_256        ((uint8_t)(0xA6))       //Page size 256 bytes command
-#define SIZE_264        ((uint8_t)(0xA7))       //Page size 256 bytes command 
+#define SIZE_264        ((uint8_t)(0xA7))       //Page size 264 bytes command
 
 #define RESTART         ((uint32_t)(0x000000f0))//Software Reset
 
@@ -72,111 +73,108 @@ PA7     ------> SPI1_MOSI   */
 #define DUMMY8B         ((uint8_t)(0x00))       //8 dummy bits
 
 #define PGERASE         ((uint8_t)(0x81))       //Page erase
-#define BLOCK_ERASE     ((uint8_t)(0x50))       //Block erase 
-#define FULL_ERASE      ((uint32_t)(0x9A8094C7))//Chip Erase    
+#define BLOCK_ERASE     ((uint8_t)(0x50))       //Block erase
+#define FULL_ERASE      ((uint32_t)(0x9A8094C7))//Chip Erase
 
-
-//Карта памяти работы с файлами
-#define ALL_PAGES      (32768UL)               //страниц памяти от 0 - 32767
-#define ALL_SIZE       (ALL_PAGES*256UL)       //общий размер в байтах 
+//Memory map
+#define ALL_PAGES      (32768UL)
+#define ALL_SIZE       (ALL_PAGES*256UL)
 #define WRITE_ADT_ST   (0x00)
 #define PAGE_SIZE      (256U)
 #define ONE_MB         (1024U)
 #define PAGE_X_2       (512U)
 
+//see datasheet (Chip Erase Time) + 2 sec
+#define MAX_FULL_ERASE_TIME  (210U * 1000U)
 
-//смотри datasheet (Chip Erase Time) + 2 sec
-#define MAX_FULL_ERASE_TIME  (210U * 1000U) //максимум стирания 208 сек(тут в мсек)
-
-#define AT45_ERR        (0)
-#define AT45_OK         (1)
-//#define AT45_BUSY       (2)
-
-#define MAX_FILES       (100)                   //Количество треков и инфы о них
-
-
-
-
-
-
-  
+typedef enum
+{
+  AT_45_OK = 0,
+  AT_45_ER_PAR,
+  AT_45_SPI_BUSY,
+  AT_45_SPI_TIMOUT,
+  AT_45_COMM_ERR
+}eAt45status;
 
 
 /**/
-void SPI1_Init(void);
+void at45db_init(void);
 
-/* Чтение идентификационной информации */
-uint8_t ad45_test(void);
+/* Reading identification information */
+eAt45status at45_test(void);
 
-/* Прочитать регистр статуса */
+/*
+Read status register */
 uint16_t at45_read_status(void);
 
-/* Разбор ргистра статуса */
+/* Parsing the status register */
 void parsing_at45_status(uint16_t tb_sr);
 
-/* Сброс at45, используется для приостановки операций */
-uint8_t at45_reset(void);
+/* Reset at45, used to suspend operations */
+eAt45status at45_reset(void);
 
-/* Функция проверки готовности к работе */
-uint8_t at45_ready(void);
+/* checking ready to work */
+eAt45status at45_ready(void);
 
-/* Устанновка размера страницы 256/264 */
-uint8_t at45_set_ps(uint8_t opc);
+/* Page size setting 256/264 */
+eAt45status at45_set_ps(uint8_t opc);
 
-/* Проверка размера страницы at45 */
+/* Page size check */
 uint8_t at45_ps_read(void);
 
-/* Стирание страницы */
-uint8_t at45_page_erase(uint16_t page);
+/* Page Erase */
+eAt45status at45_page_erase(uint16_t page);
 
-/* Полное стирание данных с at45 */
-uint8_t at45_full_erase(uint32_t * free_mem);
+/* Full erases */
+eAt45status at45_full_erase(uint32_t * free_mem);
 
-/* Загрузить данны в буфер 1 или 2 */
-uint8_t at45_load_to_buff(const uint8_t * data,
-                          const uint32_t len,
-                          const uint8_t addr,
-                          const uint8_t bufn);
+/* Load data into buffer 1 or 2 */
+eAt45status at45_load_to_buff(const uint8_t * data,
+                              const uint32_t len,
+                              const uint8_t addr,
+                              const uint8_t bufn);
 
-/* Записать данные из буфера 1 или 2 в основную память */
-uint8_t at45_write_to_main(const uint16_t page,
-                           const uint8_t bufn,
-                           const uint8_t er);
+/* Write data from buffer 1 or 2 to main memory*/
+eAt45status at45_write_to_main(const uint16_t page,
+                               const uint8_t bufn,
+                               const uint8_t er);
 
-/* Записать сразу 2 страницы */
-uint8_t at45_2page_write(uint16_t page, uint8_t * src, uint32_t len);
+/* Record 2 page at once */
+eAt45status at45_2page_write(uint16_t page, uint8_t * src, uint32_t len);
 
-/* Запись данных в страницу at45 целого буфера(1 или 2) без стирания */
-uint8_t at45_page_write_noer(const uint16_t page, 
-                             const uint8_t addr,
-                             const uint8_t * src, 
-                             const uint32_t len,
-                             const uint8_t bufn);
+/* Write data to the at45 page of the whole buffer (1 or 2) without erasing */
+eAt45status at45_page_write_noer(const uint16_t page,
+                                 const uint8_t addr,
+                                 const uint8_t * src,
+                                 const uint32_t len,
+                                 const uint8_t bufn);
 
-/* Запись данных в страницу at45 целого буфера(1 или 2) с предварительным 
-стиранием страницы*/
-uint8_t at45_page_write_er(const uint16_t page, 
+/* Writing data to the at45 page of the whole buffer (1 or 2) with a pre page erase */
+eAt45status at45_page_write_er(const uint16_t page,
+                               const uint8_t addr,
+                               const uint8_t * src,
+                               const uint32_t len,
+                               const uint8_t bufn);
+
+/* Reading data from an at45 page at the address of the page and the byte in it */
+eAt45status at45_read_page(const uint16_t page,
                            const uint8_t addr,
-                           const uint8_t * src, 
-                           const uint32_t len,
-                           const uint8_t bufn);
+                           uint8_t * dst,
+                           const uint32_t len);
 
-/* Чтение данных из страницы at45 по адресу страницы и байта в ней */
-uint8_t at45_read_page(const uint16_t page, 
-                       const uint8_t addr,
-                       uint8_t * dst,
-                       const uint32_t len);
+/* Read data from at45 page at byte address from 0 to MAX_BYTE_ADDR */
+eAt45status at45_read_byte_adr(uint8_t * dst,
+                               const uint32_t addr,
+                               const uint32_t len);
 
-/* Чтение данных из страницы at45 по адресу байта от 0 до MAX_BYTE_ADDR */
-uint8_t at45_read_byte_adr(uint8_t * dst,
-                       const uint32_t addr,
-                       const uint32_t len);
+/* Write data from at45 page to byte address from 0 до MAX_BYTE_ADDR */
+eAt45status at45_write_byte_adr(uint8_t * dst,
+                                const uint32_t addr,
+                                const uint32_t len);
 
-/* Запись данных из страницы at45 по адресу байта от 0 до MAX_BYTE_ADDR */
-uint8_t at45_write_byte_adr(uint8_t * dst,
-                       const uint32_t addr,
-                       const uint32_t len);
-
-/**/
-
+/* Low levlel functions */
+extern uint32_t GetTick(void);
+extern void delay_ms(uint32_t d);
+eAt45status spi_transmit(const uint8_t * data, size_t size, uint32_t timeout);
+eAt45status spi_recive(uint8_t * rdata, size_t size, uint32_t timeout);
 #endif
